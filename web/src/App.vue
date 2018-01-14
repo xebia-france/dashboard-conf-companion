0e4b8ffcb0bf33f023a00b92155c59011b0647dd
Benjamin Lacroix <benjlacroix@gmail.com>
2018-01-14 17:30:23 +0100

    copy Google form design

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,23 +1,56 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header></header>
+    <section id="body">
+      <div class="separator"></div>
+      <router-view></router-view>
+    </section>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+  export default {
+    name: 'app',
+  };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  $main-color: #2c374c;
+
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #f1f1f1;
+    text-align: center;
+    font-family: 'Lato', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  h1, h2, h3, h4, h5 {
+    font-family: 'Oswald', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  #app {
+    header {
+      background-color: $main-color;
+      width: 100%;
+      height: 125px;
+    }
+
+    #body {
+      width: 90%;
+      margin: -50px auto;
+      background-color: #ffffff;
+      max-width: 800px;
+
+      .separator {
+        height: 10px;
+        width: 100%;
+        background-color: lighten($main-color, 30%);
+      }
+    }
+  }
 </style>
---
+++ web/src/components/Answers.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Feedback XKE</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Answers',
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
