f1cec90be612dcf485af9f6a4e6bd9b611d4c3d6
Benjamin Lacroix <b.lacroix@betclicgroup.com>
2018-05-18 10:17:02 +0200

    chore(*): enable flavor for dataxday

--- web/config/prod.env.js
+++ web/config/prod.env.js
@@ -1,5 +1,8 @@
-'use strict'
-module.exports = {
+'use strict';
+
+const merge = require('webpack-merge');
+
+const env = {
   NODE_ENV: '"production"',
   FIREBASE_CONFIGURATION: JSON.stringify({
     apiKey: 'AIzaSyD9ftH-gF7h4UrRD_7aPN3GOpO-k2LFuVM',
@@ -8,5 +11,11 @@ module.exports = {
     projectId: 'conf-companion',
     storageBucket: 'conf-companion.appspot.com',
     messagingSenderId: '208414546348',
-  })
+  }),
+};
+
+if (process.env.FLAVOR) {
+  module.exports = merge(env, require(`./${process.env.FLAVOR}.flavor.js`));
+} else {
+  module.exports = env;
 }
